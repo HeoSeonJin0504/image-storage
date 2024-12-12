3bb0539425887d2c6ff0f1ba30d6c4709454a019
DESKTOP-7RQ494B\HeoSeonJin <sonjin54@naver.com>
2024-12-12 11:01:53 +0900

    프론트 1차 완성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,7 +24,7 @@ const Container = styled.div`
     font-size: 4em;
     text-align: center;
     font-family: "CormorantGaramond-BoldItalic", Arial, sans-serif;
-    background: linear-gradient(45deg, #8b0000, #b22222, #006400);
+    background: linear-gradient(45deg, #8b4513, #d2b48c, #deb887);
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
   }
